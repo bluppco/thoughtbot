565eeb83a4e71476db4cf40af8de4f2c0c52a551
Tanish Sharma <tanishsharmaab17@gmail.com>
2023-11-23 17:16:06 +0530

    chores

--- src/layouts/Container.jsx
+++ src/layouts/Container.jsx
@@ -1,7 +1,7 @@
 const ContainerJSX = ( props ) =>{
 
     return(
-        <section class="container mx-auto px-4 sm:px-0 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-[1190px] 2xl:max-w-screen-2xl flex flex-col gap-8">
+        <section className="container mx-auto px-4 sm:px-0 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-[1190px] 2xl:max-w-screen-2xl flex flex-col gap-8">
             { props.children }
         </section>
 
