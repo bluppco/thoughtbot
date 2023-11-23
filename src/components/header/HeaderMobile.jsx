bd6f5c74938fb2728cd676d970f430b6fc5d3c91
Tanish Sharma <tanishsharmaab17@gmail.com>
2023-11-23 15:36:00 +0530

    fix: fixed padding between components for mobile optimization

--- src/components/header/HeaderMobile.jsx
+++ src/components/header/HeaderMobile.jsx
@@ -1,46 +1,48 @@
 import { motion, AnimatePresence } from "framer-motion"
 import { useState } from "react"
-import ButtonJSX from "../../atoms/jsx/Button"
+import HeaderItemJSX from "./HeaderItem"
 
 const HeaderMobile = () => {
 
     const [ isOpen, updateOpen ] = useState( false )
 
     return (
-        <header className="md:hidden bg-white px-6 py-6 fixed z-30 w-full border-b border-thoughtbot_red">
-            <div className="flex justify-center items-center z-30 relative h-10">
+        <header className="md:hidden px-6 py-4 fixed z-30 w-full bg-white">
+            <div className="flex justify-between items-center z-30 relative h-10">
                 <a href="/">
+                <div className="w-48 aspect-[4/1]">
                     <img
                         src="/logos/logo.png"
                         alt=""
-                        className="w-20 object-cover"
+                        className="w-full h-full object-cover"
                     />
-                </a>
-                <div onClick={ () => updateOpen( !isOpen ) } className="absolute left-0">
-                    {
-
-                        isOpen &&
-                        <img
-                            src="/icons/close.svg"
-                            className="w-6 aspect-square"
-                        />
-
-                    }
-                    {
-
-                        !isOpen &&
-                        <img
-                            src="/icons/menu.svg"
-                            className="w-6 aspect-square"
-                        />
-
-                    }
                 </div>
-                <div className="absolute right-0">
-                <ButtonJSX>123</ButtonJSX>
+                </a>
+                <div className="flex gap-2 items-center">
+                    <p className="font-calibre text-thoughtbot_slate">Let's talk</p>
+                    <div onClick={ () => updateOpen( !isOpen ) }>
+                        {
+
+                            isOpen &&
+                            <img
+                                src="/icons/close.svg"
+                                className="w-6 aspect-square"
+                            />
+
+                        }
+                        {
+
+                            !isOpen &&
+                            <img
+                                src="/icons/menu.svg"
+                                className="w-6 aspect-square"
+                            />
+
+                        }
+                    </div>
                 </div>
             </div>
-            <AnimatePresence initial={false}>
+            <AnimatePresence initial={ false }>
                 {
 
                     <motion.div
@@ -49,15 +51,18 @@ const HeaderMobile = () => {
                         animate={ isOpen ? "open" : "collapsed" }
                         exit="collapsed"
                         variants={{
-                            open: { opacity: 1, height: "100vh" },
+                            open: { opacity: 1, height: "240px" },
                             collapsed: { opacity: 0, height: "0px" }
                         }}
                         transition={{ duration: 0.3 }}
                         className="grow"
                     >
-                        <div className="flex flex-col gap-1 items-center justify-center h-full z-10">
-                            <ul className="flex flex-col gap-8 text-xl text-center">
-
+                        <div className="h-full z-10 pt-10">
+                            <ul className="flex flex-col gap-6 text-center">
+                                <HeaderItemJSX>Services</HeaderItemJSX>
+                                <HeaderItemJSX>Case Studies</HeaderItemJSX>
+                                <HeaderItemJSX>Resources</HeaderItemJSX>
+                                <HeaderItemJSX>Celebrating 20 Years 🎉</HeaderItemJSX>
                             </ul>
                         </div>
                     </motion.div>
