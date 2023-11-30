5b7a03eefd29b2262fb4c29cd1d4ad015010b0a0
Tanish Sharma <tanishsharmaab17@gmail.com>
2023-11-30 12:57:30 +0530

    fix: fixed container width for xl screen

--- src/layouts/Container.jsx
+++ src/layouts/Container.jsx
@@ -1,7 +1,7 @@
 const ContainerJSX = ( props ) =>{
 
     return(
-        <section className="container mx-auto px-4 sm:px-0 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl flex flex-col gap-8">
+        <section className="container mx-auto px-4 sm:px-0 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-[1120px] mac:max-w-screen-xl 2xl:max-w-screen-2xl flex flex-col gap-8">
             { props.children }
         </section>
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,17 @@ export default {
 	theme: {
 		extend: {
 
+			screens: {
+
+				'sm': '640px',
+				'md': '768px',
+				'lg': '1024px',
+				'xl': '1280px',
+				'mac': '1440px',
+				'2xl': '1536px'
+
+			},
+
 			fontFamily: {
 
 				"calibre": [ "Calibre Regular", "Helvetica Neue", "Helvetica", "Arial", "sans-serif" ],
