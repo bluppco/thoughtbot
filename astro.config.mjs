eb5d1e4b1ba6e9e98c948acf85953d4168f35e2f
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-02-19 17:50:03 +0530

    chore: formatted the files

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,14 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), mdx()],
-  output: "server",
-  adapter: cloudflare({
-    runtime: {
-      mode: 'local',
-      type: 'pages'
-    }
-  })
+	integrations: [tailwind({
+		applyBaseStyles: false
+	}), react(), mdx()],
+	output: "server",
+	adapter: cloudflare({
+		runtime: {
+			mode: 'local',
+			type: 'pages'
+		}
+	})
 });
